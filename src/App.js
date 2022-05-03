2cc6caf363b6c04f5c4d4ee058681492b9be00aa
Ejiro David <teerex@Ejiros-MacBook-Air.local>
2022-05-03 02:41:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const title = 'welcome to the new blog';
+const likes = 50;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>App Component</h1>
+
+        <p>{title}</p>
+        <p>Liked {likes } times</p>
+        <p>[1,2,34,6,5,6,]</p>
+        <p>{Math.random() * 100}</p>
+      </div>
     </div>
   );
 }
